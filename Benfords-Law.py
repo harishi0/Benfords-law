2ed7c3c7ae6bdae919fa498d1995f99e9d67d23d
ashish345678 <350236378@gapps.yrdsb.ca>
2023-04-18 15:29:28 -0400

    commit 3

--- Benfords-Law.py
+++ Benfords-Law.py
@@ -1,13 +1,16 @@
 import os
-salesData = input("type in the file to open")
-correctFile = "sales.csv"
 def printfile():
     with open('sales.csv') as file:
         print(file.read())
-if salesData == correctFile:
-     printfile()
-else:
-    print("invalid file")
+
+salesData = input("type in the file to open")
+correctFile = "sales.csv"
+
+while salesData != correctFile:
+    print("invalid file, try again!")
+    salesData = input("type in the file to open")
+
+printfile()
 
     
 
