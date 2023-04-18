820adfe1c0bc77ea5703edeb77c14ab6dbbce3ec
ashish345678 <350236378@gapps.yrdsb.ca>
2023-04-18 15:00:18 -0400

    change 1

--- Benfords-Law.py
+++ Benfords-Law.py
@@ -1,3 +1,12 @@
-salesData = input("press 1 to open the data file")
+import os
+salesData = input("type in the file to open")
+correctFile = "sales.csv"
+
+if salesData == correctFile:
+    folder = os.getcwd
+    file = folder + "\\BENFORDS-LAW\\sales.csv"
+    filename = open(file, "r")
+    
+
 
 
