411e1c60814540460f2aaa05f6da1c346d1dd244
harishi0 <harishjbuvan@gmail.com>
2023-04-21 15:18:04 -0400

    Part 2 a code

--- testing.py
+++ testing.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import math
 import os
 # Define a function that extracts the first digit of a number
@@ -20,11 +19,4 @@ first_digits = [first_digit(int(line.split(",")[1])) for line in data[1:]]
 expected_freqs = [math.log10(1 + 1/d) for d in range(1, 10)]
 # Calculate the percentages
 actual_freqs = [first_digits.count(d) / len(first_digits) for d in range(1, 10)]
-
-# Plot the expected and actual frequencies
-plt.bar(range(1, 10), expected_freqs, color='b', alpha=1, label='Benford')
-plt.bar(range(1, 10), actual_freqs, color='r', alpha=1, label='Actual')
-plt.xlabel('First Digit')
-plt.ylabel('Percentage')
-plt.legend()
-plt.show()
+print (actual_freqs)
