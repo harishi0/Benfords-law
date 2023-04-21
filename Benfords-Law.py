f594cb1240d020d24136b50b822107262665a728
harishi0 <harishjbuvan@gmail.com>
2023-04-21 18:30:21 -0400

    asdfghjk,./

--- Benfords-Law.py
+++ Benfords-Law.py
@@ -6,16 +6,16 @@ def printfile():
         print(file.read())
     
 
-salesData = input("type in the file to open")
+salesData = input("type in the file to open:")
 correctFile = "sales.csv"
 
 while salesData != correctFile:
     print("invalid file, try again!")
-    salesData = input("type in the file to open")
+    salesData = input("type in the file to open:")
 
 printfile()
 
-validate = input("press 2 to validate")
+validate = input("press 2 to validate:")
 correctInp = "2"
 
 def validateNumber():
