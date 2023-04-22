e854d6282f6b0bf86d95775940ac81848831a919
harishi0 <harishjbuvan@gmail.com>
2023-04-22 18:33:44 -0400

    agbufhsnksfz

--- Benfords-Law.py
+++ Benfords-Law.py
@@ -1,5 +1,5 @@
 import os
-import math
+import matplotlib as plt
 #printing file data onto the terminal
 def printfile():
     with open('sales.csv') as file:
@@ -17,6 +17,17 @@ def validateNumber():
     Calc_freq = [round(first_digits.count(digits) / len(first_digits)*100, 2,) for digits in range(1, 10)]
     return Calc_freq
 
+#create the graph
+def createGraph():
+    digits = range(1, 10)
+    freq = validateNumber()
+    plt.bar(digits, freq)
+    plt.xlabel('First Digit')
+    plt.ylabel('Frequency (%)')
+    plt.title('Benford\'s Law Distribution')
+    plt.show()
+
+
 def FraudCheck():
     First_digit_freq = validateNumber()
     if 29 <= First_digit_freq[0] <= 32:
@@ -31,8 +42,7 @@ def Print_csv(data, filename):
         file.write('Digit,Frequency\n')
         for digi, freq in zip(range(1, 10), data):
             file.write(str(digi) + ' = ' + str(freq) + '\n')
-    print(f'Digit frequency data written to {filename}.')
-
+    print(f'Digit frequency writen to {filename}.')
 
 
 salesData = input("type in the file to open:")
@@ -51,6 +61,8 @@ while validate != correctInp:
     validate = input("press 2 to validate")
 
 validateNumber()
+
+
 FraudCheck()
 
 csv_print= input("type 3 to see results")
